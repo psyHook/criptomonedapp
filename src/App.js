ed5dcbae18ce7bbaa39bacca2be1721379ce286d
Juan Galarce <j6alarce@gmail.com>
2021-01-27 12:00:25 -0300

    Se crea el componente formulario y su styled component correspondiente

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import imagen from "./assets/cryptomonedas.png";
+import Formulario from './components/Formulario';
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -43,6 +44,7 @@ function App() {
       </div>
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario></Formulario>
       </div>
     </Contenedor>
   );
